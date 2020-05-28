c0364b601b6499ab1a222485165324d76646b355
tengfei <arrayadapter.cn@gmail.com>
2020-05-28 20:32:04 +0800

    update

---
+++ GeekDataStructureAndAlgorithm/src/main/java/com/example/tengfei/day10/Decompilation.java
@@ -0,0 +1,16 @@
+package com.example.tengfei.day10;
+
+/**
+ * @author by tengfei on 2020/5/28.
+ * @description：
+ */
+public class Decompilation {
+
+    private Object object = new Object();
+
+    public void insert(Thread thread){
+        synchronized (object){
+
+        }
+    }
+}
