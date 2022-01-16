c642c9d01189084887cb9afb54062e177426f311
滕飞 <arrayadapter.cn@gmail.com>
2022-01-16 21:10:16 +0800

    刷题

---
+++ Agorithm/src/main/kotlin/Question27.kt
@@ -0,0 +1,13 @@
+/**
+ * 27. 移除元素
+ */
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    var showIndex = 0
+    for((index,element) in nums.withIndex()){
+        if(`val`!=element){
+            nums[showIndex++] = element
+        }
+    }
+    return  showIndex
+}
+
--- Agorithm/src/main/kotlin/Question34.kt
+++ Agorithm/src/main/kotlin/Question34.kt
@@ -1,4 +1,49 @@
-
+/**
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ */
 fun searchRange(nums: IntArray, target: Int): IntArray {
+    var leftBorder = getLeftBorder(nums,target)
+    var rightBorder = getRightBorder(nums,target)
+    if(leftBorder == -2 || rightBorder == -2){
+        return intArrayOf(-1,-1)
+    }
+    if(rightBorder - leftBorder > 1){
+        return intArrayOf(leftBorder+1,rightBorder-1)
+    }
+    return intArrayOf(-1,-1)
+}
 
+/**
+ * 在排序数组中查找元素的第一个和最后一个位置
+ */
+fun getRightBorder(nums: IntArray, target: Int):Int{
+    var left = 0
+    var right = nums.size - 1
+    var rightBorder = -2;
+    while (left <= right){
+        var middle = left+((right - left)/2)
+        if (nums[middle] > target ){
+            right = middle - 1
+        }else{
+            left = middle +1
+            rightBorder = left
+        }
+    }
+    return rightBorder
+}
+
+fun getLeftBorder(nums: IntArray, target: Int):Int{
+    var left = 0
+    var right = nums.size - 1
+    var leftBorder = -2;
+    while (left <= right){
+        var middle = left+((right - left)/2)
+        if (nums[middle] >= target ){
+            right = middle - 1
+            leftBorder = right
+        }else{
+            left = middle +1
+        }
+    }
+    return leftBorder
 }
