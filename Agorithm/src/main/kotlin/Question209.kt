0522bf2da041526db86cca35bf8144665f34853e
滕飞 <arrayadapter.cn@gmail.com>
2022-01-17 17:02:58 +0800

    209. 长度最小的子数组 977. 有序数组的平方

---
+++ Agorithm/src/main/kotlin/Question209.kt
@@ -0,0 +1,25 @@
+/**
+ * 209.长度最小的子数组
+ * 滑动窗口
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+ * 题解：https://programmercarl.com/0209.%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84.html#_209-%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84
+ */
+fun minSubArrayLen(target: Int, nums: IntArray): Int {
+    var result = Int.MAX_VALUE
+    //滑动窗口的起始位置
+    var start = 0
+    //计算的值的总和
+    var sum = 0
+    //滑动窗口的长度
+    var subLength = 0
+    //循环遍历数组
+    for ((index,element) in nums.withIndex()){
+        sum+=element
+        while (sum >= target){
+            subLength = (index - start +1)
+            result = if (result<subLength) result else subLength
+            sum-=nums[start++]
+        }
+    }
+    return if (result == Int.MAX_VALUE) 0 else result
+}
---
+++ Agorithm/src/main/kotlin/Question977.kt
@@ -0,0 +1,22 @@
+/**
+ * 977. 有序数组的平方
+ * https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+ * 题解：https://programmercarl.com/0977.%E6%9C%89%E5%BA%8F%E6%95%B0%E7%BB%84%E7%9A%84%E5%B9%B3%E6%96%B9.html#%E5%8F%8C%E6%8C%87%E9%92%88%E6%B3%95
+ */
+
+fun sortedSquares(nums: IntArray): IntArray {
+    var left = 0
+    var right = nums.size - 1
+    var dynamicArray = IntArray(nums.size,{0})
+    var index = dynamicArray.size - 1
+    while (left <= right){
+        if(nums[right] * nums[right] > nums[left]*nums[left]){
+            dynamicArray[index--] = nums[right] * nums[right]
+            right--
+        }else{
+            dynamicArray[index--] = nums[left] * nums[left]
+            left++
+        }
+    }
+    return dynamicArray
+}
